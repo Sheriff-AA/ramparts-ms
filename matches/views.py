9a2cecffd0661afcec619c65d9a840b68f9f6227
Sherif <sherifatitebi@gmail.com>
2025-03-31 04:59:16 +0100

    display for games.i think is working

--- matches/views.py
+++ matches/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from django.views import generic
 from django.db.models import Q
 
@@ -16,7 +17,61 @@ class DisplayGamesListView(generic.ListView):
         query = self.request.GET.get('q')
         if query:
             queryset = queryset.filter(
-                Q(opposition_name__icontains=query)
+                Q(opposition_team__icontains=query)
             )
         return queryset
+    
+    def manual_pagination(self, request, queryset):
+        queryset_paginator = Paginator(queryset, 25)
+        queryset_page_number = request.GET.get("page")
+        return queryset_paginator.get_page(queryset_page_number)
+    
+    def get(self, request, *args, **kwargs):
+        self.object_list = self.get_queryset()
+        context = self.get_context_data()
+
+        search = request.GET.get('search', None)
+        date = request.GET.get('match_date', None)
+        list_type = self.request.GET.get("list_type", "results")
+
+        if list_type == "fixtures":
+            queryset = fixtures = Match.objects.filter(is_fixture=True).order_by('match_date')
+        else:
+            queryset = Result.objects.all().order_by('-match__match_date')
+        
+
+        if search:
+            if list_type == "fixtures":
+                queryset = queryset.filter(Q(opposition_team__icontains=search))
+            else:
+                queryset = queryset.filter(Q(match__opposition_team__contains=search))
+        if date:
+            if list_type == "fixtures":
+                queryset = queryset.filter(Q(match_date__date=date))
+            else:
+                queryset = queryset.filter(Q(match__match_date__date=date))
+
+        
+        queryset = self.manual_pagination(request, queryset)
+
+
+
+        context.update({
+            "queryset": queryset,
+            "list_type": list_type,
+            "search": self.request.GET.get("search", None),
+        })
+
+        if request.htmx:
+            if list_type == "fixtures":
+                template_name = 'matches/partials/display_match_fixtures.html'
+            else:
+                template_name = 'matches/partials/display_match_results.html'
+            # if search or date:  
+            #     template_name = 'matches/partials/partial_match_list.html'
+                
+            return render(request, template_name, context)
+        else:
+            template_name = 'matches/display_matches.html'
+            return render(request, template_name, context)
     
